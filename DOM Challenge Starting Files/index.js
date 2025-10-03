cfea030d237f80fa6e613b8e591812616dc67c86
rakshita-bhat <rakshitabhat2004@gmail.com>
2025-10-03 13:28:59 +0530

    Change of remote

---
+++ DOM Challenge Starting Files/index.js
@@ -0,0 +1,6 @@
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Rakshita";
+document.querySelector("li a").style.color = "red";
+document.querySelector("button").style.backgroundColor = "yellow";
+
+
+document.querySelector("h1").classList.add("huge");
