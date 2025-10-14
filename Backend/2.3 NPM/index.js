1516d41484bbeced7cb2fe298ae10143049a499a
rakshita-bhat <rakshitabhat2004@gmail.com>
2025-10-14 22:25:07 +0530

    NodeJs

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,9 @@
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}`);
+
+
+import {randomSuperhero} from 'superheroes';
+var mySuperHero = randomSuperhero();
+console.log(`I am ${mySuperHero}`);
