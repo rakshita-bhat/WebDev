8903a885165f08ffc0e2b6055e59a279f51b6e06
rakshita-bhat <rakshitabhat2004@gmail.com>
2026-01-29 10:33:23 +0530

    Authentication Level 1

---
+++ Backend/9.1 Authentication Lv.1/index.js
@@ -0,0 +1,78 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "authentication",
+  password: "Raksh@122",
+  port: 5432,
+});
+db.connect();
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+  try {
+    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+    if (checkResult.rows.length > 0) {
+      return res.send("Email already exists, Try logging in.");
+    } else {
+      db.query("INSERT INTO users (email,password) VALUES ($1,$2)", [
+        email,
+        password,
+      ]);
+      res.render("secrets.ejs");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+
+  try{
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+    if(result.rows.length < 0){
+      return res.send("No user found with this email.");
+    }else{
+      const user = result.rows[0];
+      if(user.password === password){
+        res.render("secrets.ejs");
+      }else{
+        res.send("Incorrect password.");
+      }
+    }
+  }catch(err){
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
